ec92f9762f617345c2713d19a1680bbddd3c7043
Christian Dräger <christian.draeger1@gmail.com>
2020-01-09 21:45:04 +0100

    pimp build with auto updating dependencies and docker image creation

---
+++ DEVELOPMENT.md
@@ -0,0 +1,31 @@
+pikture2text
+============
+
+## Prerequisites
+
+* jdk >= 8 installed
+
+## Recommended
+
+* gdub installed
+
+## build project
+
+    gw build
+    
+## run tests
+
+    gw check
+    
+## run project
+
+    gw bootRun
+    
+## build docker image
+
+    gw dockerBuildImage
+    
+### start application as docker container
+
+    docker run -p 8080:8080 pikture2text
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+@file:Suppress("UNUSED_VARIABLE")
+
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -5,11 +8,22 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
+	id("se.patrikerdes.use-latest-versions") version "0.2.13"
+	id("com.github.ben-manes.versions") version "0.27.0"
+	id("com.adarshr.test-logger") version "2.0.0"
+	id("com.bmuschko.docker-spring-boot-application") version "6.1.1"
 }
 
 group = "it.skrape"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+val isIdea = System.getProperty("idea.version") != null
+
+testlogger {
+	setTheme(if (isIdea) "plain" else "mocha")
+	slowThreshold = 1000
+}
 
 repositories {
 	jcenter()
@@ -36,13 +50,39 @@ dependencies {
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks {
+	withType<Test> {
+		useJUnitPlatform()
+	}
+
+	withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "1.8"
+		}
+	}
+
+	withType<DependencyUpdatesTask> {
+
+		gradleReleaseChannel = "current"
+
+		rejectVersionIf {
+			val isFlaggedAsNonStable = listOf("alpha", "beta", "RC", "rc", "M1").any { candidate.version.contains(it) }.not()
+			val isSemanticVersion =  "^[0-9,.v-]+(-r)?$".toRegex().matches(candidate.version)
+			(isFlaggedAsNonStable || isSemanticVersion).not()
+		}
+	}
+
+	val updateDependencies by creating {
+		dependsOn(useLatestVersions, test)
+	}
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+docker {
+	springBootApplication {
+		baseImage.set("openjdk:8-alpine")
+		maintainer.set(rootProject.name)
+		images.set(setOf("${rootProject.name}:latest"))
+		jvmArgs.set(listOf("-Dspring.profiles.active=production", "-Xmx2048m"))
 	}
 }
